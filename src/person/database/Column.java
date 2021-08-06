7b71af708f86e451fda7ead3623df94e0767088a
hsnysf <hasan.lmra@gmail.com>
2021-08-06 21:59:30 +0300

    Update Column.java

--- src/person/database/Column.java
+++ src/person/database/Column.java
@@ -2,27 +2,11 @@ package person.database;
 
 public class Column {
 
-	private String name;
-	private int type;
+	public String name;
+	public int type;
 	
 	public Column(String name, int type) {
 		this.name = name;
-		this.type = type;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getType() {
-		return type;
-	}
-	
-	public void setType(int type) {
 		this.type = type;
 	}
 }
