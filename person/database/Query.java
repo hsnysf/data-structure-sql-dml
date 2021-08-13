14e81be00fa5be325af414fd773aeb6881f19b64
hsnysf <hasan.lmra@gmail.com>
2021-08-13 22:34:33 +0300

    Add files via upload

--- person/database/Query.java
+++ person/database/Query.java
@@ -22,6 +22,167 @@ public class Query {
 		this.connection = connection;
 	}
 	
+	public void setObject(PreparedStatement statement, int index, int type, Object value) throws SQLException {
+		
+		System.out.println("Param #" + index + " :: " + value);
+		
+		if(type == Types.VARCHAR){
+			
+			if (value != null && !"".equals(value.toString().trim())) {
+				statement.setString(index, value.toString().trim());
+			} else {
+				statement.setNull(index, Types.VARCHAR);
+			}
+			
+		}else if(type == Types.CHAR){
+			
+			if (value != null) {
+				statement.setString(index, String.valueOf(value.toString().trim()));
+			} else {
+				statement.setNull(index, Types.CHAR);
+			}
+
+		}else if(type == Types.SMALLINT){
+			
+			if(value != null && !value.equals(0)) {
+				statement.setShort(index, Short.parseShort(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.SMALLINT);
+			}
+			
+		}else if(type == Types.INTEGER){
+			
+			if(value != null && !value.equals(0)) {
+				statement.setInt(index, Integer.parseInt(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.INTEGER);
+			}
+			
+		}else if(type == Types.BIGINT){
+			
+			if(value != null && !value.equals(0)) {
+				statement.setLong(index, Long.parseLong(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.BIGINT);
+			}
+			
+		}else if(type == Types.FLOAT){
+			
+			if(value != null && !value.equals(0)) {
+				statement.setFloat(index, Float.parseFloat(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.FLOAT);
+			}
+		
+		}else if(type == Types.DOUBLE){
+			
+			if(value != null && !value.equals(0)) {
+				statement.setDouble(index, Double.parseDouble(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.DOUBLE);
+			}
+		
+		}else if(type == Types.DECIMAL){
+			
+			if(value != null && !value.equals(0)) {
+				statement.setBigDecimal(index, new BigDecimal(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.DECIMAL);
+			}
+		
+		}else if(type == Types.DATE){
+			
+			if(value != null) {
+				statement.setDate(index, Date.valueOf(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.DATE);
+			}
+			
+		}else if(type == Types.TIMESTAMP){
+			
+			if(value != null) {
+				statement.setTimestamp(index, Timestamp.valueOf(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.TIMESTAMP);
+			}
+		
+		}else if(type == Types.TIME){
+			
+			if(value != null) {
+				statement.setTime(index, Time.valueOf(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.TIME);
+			}
+			
+		}else if(type == Types.BOOLEAN){
+			
+			if(value != null) {
+				statement.setBoolean(index, Boolean.parseBoolean(value.toString().trim()));
+			}else {
+				statement.setNull(index, Types.BOOLEAN);
+			}
+		}
+	}
+	
+	public Object getValue(String value, int type) {
+		
+		Object result = null;
+		
+		if(value != null) {
+			
+			if(type == Types.VARCHAR){
+				
+				result = value.trim();
+				
+			}else if(type == Types.CHAR){
+				
+				result = value.trim().charAt(0);
+
+			}else if(type == Types.SMALLINT){
+				
+				result = Short.parseShort(value.trim());
+				
+			}else if(type == Types.INTEGER){
+				
+				result = Integer.parseInt(value.trim());
+				
+			}else if(type == Types.BIGINT){
+				
+				result = Long.parseLong(value.trim());
+				
+			}else if(type == Types.FLOAT){
+				
+				result = Float.parseFloat(value.trim());
+			
+			}else if(type == Types.DOUBLE){
+				
+				result = Double.parseDouble(value.trim());
+			
+			}else if(type == Types.DECIMAL){
+				
+				result = new BigDecimal(value.trim());
+			
+			}else if(type == Types.DATE){
+				
+				result = Date.valueOf(value.trim());
+				
+			}else if(type == Types.TIMESTAMP){
+				
+				result = Timestamp.valueOf(value.trim());
+			
+			}else if(type == Types.TIME){
+				
+				result = Time.valueOf(value.trim());
+				
+			}else if(type == Types.BOOLEAN){
+				
+				result = Boolean.parseBoolean(value.trim());
+			}
+		}
+		
+		return result;
+	}
+	
 	public Query insertInto(Table table) {
 
 		this.table = table;
@@ -142,108 +303,8 @@ public class Query {
 			}
 		}
 		
+		System.out.println("Generated ID :: " + id);
+		
 		return id;
-	}
-	
-	public void setObject(PreparedStatement statement, int index, int type, Object value) throws SQLException {
-		
-		System.out.println("Param #" + index + " :: " + value);
-		
-		if(type == Types.VARCHAR){
-			
-			if (value != null && !"".equals(value.toString().trim())) {
-				statement.setString(index, (String)value);
-			} else {
-				statement.setNull(index, Types.VARCHAR);
-			}
-			
-		}else if(type == Types.CHAR){
-			
-			if (value != null) {
-				statement.setString(index, String.valueOf(value));
-			} else {
-				statement.setNull(index, Types.CHAR);
-			}
-
-		}else if(type == Types.SMALLINT){
-			
-			if(value != null && (short)value != 0) {
-				statement.setShort(index, (short)value);
-			}else {
-				statement.setNull(index, Types.SMALLINT);
-			}
-			
-		}else if(type == Types.INTEGER){
-			
-			if(value != null && (int)value != 0) {
-				statement.setInt(index, (int)value);
-			}else {
-				statement.setNull(index, Types.INTEGER);
-			}
-			
-		}else if(type == Types.BIGINT){
-			
-			if(value != null && (long)value != 0) {
-				statement.setLong(index, (long)value);
-			}else {
-				statement.setNull(index, Types.BIGINT);
-			}
-			
-		}else if(type == Types.FLOAT){
-			
-			if(value != null && (float)value != 0) {
-				statement.setFloat(index, (float)value);
-			}else {
-				statement.setNull(index, Types.FLOAT);
-			}
-		
-		}else if(type == Types.DOUBLE){
-			
-			if(value != null && (double)value != 0) {
-				statement.setDouble(index, (double)value);
-			}else {
-				statement.setNull(index, Types.DOUBLE);
-			}
-		
-		}else if(type == Types.DECIMAL){
-			
-			if(value != null && !((BigDecimal)value).equals(0)) {
-				statement.setBigDecimal(index, (BigDecimal)value);
-			}else {
-				statement.setNull(index, Types.DECIMAL);
-			}
-		
-		}else if(type == Types.DATE){
-			
-			if(value != null) {
-				statement.setDate(index, (Date)value);
-			}else {
-				statement.setNull(index, Types.DATE);
-			}
-			
-		}else if(type == Types.TIMESTAMP){
-			
-			if(value != null) {
-				statement.setTimestamp(index, (Timestamp)value);
-			}else {
-				statement.setNull(index, Types.TIMESTAMP);
-			}
-		
-		}else if(type == Types.TIME){
-			
-			if(value != null) {
-				statement.setTime(index, (Time)value);
-			}else {
-				statement.setNull(index, Types.TIME);
-			}
-			
-		}else if(type == Types.BOOLEAN){
-			
-			if(value != null) {
-				statement.setBoolean(index, (boolean)value);
-			}else {
-				statement.setNull(index, Types.BOOLEAN);
-			}
-		}
 	}
 }
